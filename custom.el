45e8509e0c49b6f64fe4c219e769fde3c3e1793b
Dzianis Bogdan <bogdan.denv@gmail.com>
2024-01-02 18:09:51 +0300

    feat: ivy, rainbow, doom-modeline

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package zenburn-theme solarized-theme smex magit)))
+ '(package-selected-packages
+   '(rainbow-delimiters doom-modeline command-log-mode use-package zenburn-theme solarized-theme smex magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
